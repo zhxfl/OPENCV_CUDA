b8fcd27c56d6fee65c663ca7758130e9325a50dc
zhxfl <acer@zhxfl.(none)>
2014-03-03 13:46:13 +0800

    溢出

--- sobel/CppSobel.cpp
+++ sobel/CppSobel.cpp
@@ -13,6 +13,7 @@ void CppSobel(IplImage* in, IplImage *out,
 	uchar *inPtr4 = NULL;
 	uchar *inPtr5 = NULL;
 	uchar *inPtr6 = NULL;
+	uchar *outPtr = NULL;
 
 	int width	  = in->width;
 	int height    = in->height;
@@ -24,6 +25,8 @@ void CppSobel(IplImage* in, IplImage *out,
 	int *pEdgeXPtr = NULL;
 	int *pEdgeYPtr = NULL;
 	uchar *edgPtr = NULL;
+	int edgeXMax = 0;
+	int edgeYMax = 0;
 
 	const int widH = 1;
 	const int widV = widthStep;
@@ -42,8 +45,9 @@ void CppSobel(IplImage* in, IplImage *out,
 			for (j=1;j<width-1;j++, pEdgeXPtr++)
 			{
 				*pEdgeXPtr
-					= (*inPtr1++ * 1 + (int)*inPtr2++ * 2 + *inPtr3++ * 1)
-					- (*inPtr4++ * 1 + (int)*inPtr5++ * 2 + *inPtr6++ * 1);
+					= (*inPtr1++ + (int)*inPtr2++ * 2 + *inPtr3++)
+					- (*inPtr4++ + (int)*inPtr5++ * 2 + *inPtr6++);
+				//edgeXMax = std::min(edgeXMax, *pEdgeXPtr);
 			}
 		}
 	}
@@ -53,7 +57,7 @@ void CppSobel(IplImage* in, IplImage *out,
 		for (i=1;i<height-1;i++)
 		{
 			pEdgeYPtr = pEdgeY + i*width + 1;
-			inPtr = (uchar*)in->imageData + i * widthStep + 1;
+			inPtr  = (uchar*)in->imageData + i * widthStep + 1;
 			inPtr1 = inPtr + widV - widH;
 			inPtr2 = inPtr + widV;
 			inPtr3 = inPtr + widV + widH;
@@ -70,9 +74,22 @@ void CppSobel(IplImage* in, IplImage *out,
 	}
 	if(xorder && ! yorder)
 	{
-		for(i = 0; i < height; i++)
+		for (i=1;i<height-1;i++)
 		{
-			strcpy(out->imageData + i * widthStep, (char*)pEdgeX + i * width);
+			pEdgeXPtr = pEdgeX + i*width + 1;
+			inPtr = (uchar*)in->imageData + i * widthStep + 1;
+			outPtr = (uchar*)out->imageData + i * widthStep + 1;
+			inPtr1 = inPtr + widH - widV;
+			inPtr2 = inPtr + widH;
+			inPtr3 = inPtr + widH + widV;
+			inPtr4 = inPtr - widH - widV;
+			inPtr5 = inPtr - widH;
+			inPtr6 = inPtr - widH + widV;
+			for (j=1;j<width-1;j++, pEdgeXPtr++)
+			{
+				*outPtr = 255.0f * (*pEdgeXPtr) / edgeXMax;
+				outPtr++;
+			}
 		}
 	}
 	else if(!xorder && yorder)
--- sobel/main.cpp
+++ sobel/main.cpp
@@ -2,9 +2,26 @@
 #include "opencv/cv.h"
 #include "opencv2/contrib/contrib.hpp"
 #include "CppSobel.h"
+#include <iostream>
 
-void runCppSobel(IplImage *pIn,IplImage * pOut)
+static cv::Mat loadImage(const std::string& name)
 {
+	cv::Mat image = cv::imread(name, cv::IMREAD_GRAYSCALE);
+	if (image.empty())
+	{
+		std::cerr << "Can't load image - " << name << std::endl;
+		exit(-1);
+	}
+	return image;
+}
+
+
+void runCppSobel()
+{
+	IplImage *pSrc  = cvLoadImage("1.png",1);
+	IplImage *pIn   = cvCreateImage(cvSize(pSrc->width, pSrc->height), IPL_DEPTH_8U, 1);
+	IplImage *pOut  = cvCreateImage(cvSize(pSrc->width, pSrc->height), IPL_DEPTH_8U, 1);
+	cvCvtColor(pSrc, pIn, CV_BGR2GRAY);  // color to gray
 	cv::TickMeter tm;
 	tm.start();
 	CppSobel(pIn,pOut,1,1);
@@ -14,27 +31,25 @@ void runCppSobel(IplImage *pIn,IplImage * pOut)
 	cvShowImage("CppSobel",pOut);
 }
 
-void runCvSobel(IplImage *pIn,IplImage * pOut)
+
+void runCvSobel()
 {
+	cv::Mat _in  = loadImage("1.png");
+	cv::Mat _out;
+	_out.create(_in.size(), CV_MAKETYPE(CV_32F, _in.channels()));
 	cv::TickMeter tm;
 	tm.start();
-	cvSobel(pIn,pOut,1,1);
+	Sobel(_in, _out, CV_32F, 1, 1);
 	tm.stop();
 	printf("cvSobel  time: %4.4f ms\n", tm.getTimeMilli());
 	cvNamedWindow("cvSobel",1);
-	cvShowImage("cvSobel",pOut);
+	imshow("cvSobel",_out);
 }
 
 int main()
 {
-	IplImage *pSrc  = cvLoadImage("1.jpg",1);
-	IplImage *pIn  = cvCreateImage(cvSize(pSrc->width, pSrc->height), IPL_DEPTH_8U, 1);
-	IplImage *pOut = cvCreateImage(cvSize(pSrc->width, pSrc->height), IPL_DEPTH_8U, 1);
-	cvCvtColor(pSrc, pIn, CV_BGR2GRAY);  // color to gray
-
-	runCppSobel(pIn,pOut);
-	runCvSobel(pIn,pOut);
-
+	runCppSobel();
+	runCvSobel();
 	cvWaitKey(0);
 	return 0;
 
